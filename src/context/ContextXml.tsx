944747d2b7b83bdf1debf4d0fadedd69a07453b2
Daniel Ximenes <93453046+Denionline@users.noreply.github.com>
2025-03-25 19:44:46 +0000

    feat: Add a function to change data that called on GuideDetails

--- src/context/ContextXml.tsx
+++ src/context/ContextXml.tsx
@@ -1,5 +1,11 @@
 import { createContext, useState, useEffect } from "react";
-import { GuiaSPSADT, ObjectXml, OutraDespesa, ProcedimentoExecutado } from "@/utils/XmlTypes";
+import {
+  GuiaSPSADT,
+  ObjectXml,
+  OutraDespesa,
+  ProcedimentoExecutado,
+} from "@/utils/XmlTypes";
+import { FormValuesType } from "@/pages/guide-details/page";
 
 export interface ArquiveType {
   ID: string;
@@ -15,6 +21,11 @@ export interface ArquiveType {
 export interface ContextXmlType {
   arquives: ArquiveType[] | undefined;
   AddNewArquive: (xmlInJson: ObjectXml, fileName: string) => void;
+  UpdateGuideDetails: (
+    idXml: string,
+    idxGuide: number,
+    data: FormValuesType
+  ) => void;
 }
 
 export const ContextXml = createContext({} as ContextXmlType);
@@ -42,7 +53,6 @@ export const ContextXmlProvider = ({ children }: any) => {
   }, []);
 
   function AddNewArquive(xmlInJson: ObjectXml, fileName: string) {
-    console.log(xmlInJson)
     if (
       !Array.isArray(
         xmlInJson["ans:mensagemTISS"]["ans:prestadorParaOperadora"][
@@ -63,28 +73,27 @@ export const ContextXmlProvider = ({ children }: any) => {
     ]["ans:guiasTISS"]["ans:guiaSP-SADT"] as GuiaSPSADT[];
 
     guides.map((guide) => {
-
-        if (guide["ans:procedimentosExecutados"] && 
-          !Array.isArray(
-            guide["ans:procedimentosExecutados"]["ans:procedimentoExecutado"]
-          )
-        ) {
-          const procedimento = guide["ans:procedimentosExecutados"][
-            "ans:procedimentoExecutado"
-          ] as ProcedimentoExecutado;
-          guide["ans:procedimentosExecutados"]["ans:procedimentoExecutado"] = [
-            procedimento,
-          ];
-        }
-
-
-
-      if (guide["ans:outrasDespesas"] && 
+      if (
+        guide["ans:procedimentosExecutados"] &&
         !Array.isArray(
-          guide["ans:outrasDespesas"]["ans:despesa"]
+          guide["ans:procedimentosExecutados"]["ans:procedimentoExecutado"]
         )
       ) {
-        const despesa = guide["ans:outrasDespesas"]["ans:despesa"] as OutraDespesa;
+        const procedimento = guide["ans:procedimentosExecutados"][
+          "ans:procedimentoExecutado"
+        ] as ProcedimentoExecutado;
+        guide["ans:procedimentosExecutados"]["ans:procedimentoExecutado"] = [
+          procedimento,
+        ];
+      }
+
+      if (
+        guide["ans:outrasDespesas"] &&
+        !Array.isArray(guide["ans:outrasDespesas"]["ans:despesa"])
+      ) {
+        const despesa = guide["ans:outrasDespesas"][
+          "ans:despesa"
+        ] as OutraDespesa;
         guide["ans:outrasDespesas"]["ans:despesa"] = [despesa];
       }
       return guide;
@@ -100,7 +109,6 @@ export const ContextXmlProvider = ({ children }: any) => {
         TotalValueXml: CalcValueTotalInXml(xmlInJson),
       },
     };
-    console.log(newArquive);
 
     const updatedArquives = [...arquives, newArquive];
     setArquives(updatedArquives);
@@ -126,8 +134,152 @@ export const ContextXmlProvider = ({ children }: any) => {
     return Number(guides["ans:valorTotal"]["ans:valorTotalGeral"]["_text"]);
   };
 
+  useEffect(() => {
+    if (arquives.length) {
+      localStorage.setItem("editor_xml_tiss_v1", JSON.stringify(arquives));
+    }
+  }, [arquives]);
+
+  function UpdateGuideDetails(
+    idXml: string,
+    idxGuide: number,
+    {
+      carteirinha,
+      guiaDoPrestador,
+      guiaDaOperadora,
+      dataDaAutorizacao,
+      dataDaValidadeDaSenha,
+      procedimentos,
+      expenses,
+    }: FormValuesType
+  ) {
+    const idxArquive = arquives.findIndex((arquive) => arquive.ID === idXml);
+    const guide =
+      arquives[idxArquive] &&
+      (arquives[idxArquive].objectXml["ans:mensagemTISS"][
+        "ans:prestadorParaOperadora"
+      ]["ans:loteGuias"]["ans:guiasTISS"]["ans:guiaSP-SADT"][
+        idxGuide
+      ] as GuiaSPSADT);
+    if (
+      guide["ans:dadosBeneficiario"]["ans:numeroCarteira"]._text != carteirinha
+    ) {
+      guide["ans:dadosBeneficiario"]["ans:numeroCarteira"]._text = carteirinha;
+    }
+
+    if (
+      guide["ans:cabecalhoGuia"]["ans:numeroGuiaPrestador"]._text !=
+      guiaDoPrestador
+    ) {
+      guide["ans:cabecalhoGuia"]["ans:numeroGuiaPrestador"]._text =
+        guiaDoPrestador;
+    }
+
+    if (
+      guide["ans:dadosAutorizacao"]["ans:numeroGuiaOperadora"]._text !=
+      guiaDaOperadora
+    ) {
+      guide["ans:dadosAutorizacao"]["ans:numeroGuiaOperadora"]._text =
+        guiaDaOperadora;
+    }
+
+    if (
+      guide["ans:dadosAutorizacao"]["ans:dataAutorizacao"]._text !=
+      dataDaAutorizacao
+    ) {
+      guide["ans:dadosAutorizacao"]["ans:dataAutorizacao"]._text =
+        dataDaAutorizacao;
+    }
+
+    if (
+      guide["ans:dadosAutorizacao"]["ans:dataValidadeSenha"] &&
+      guide["ans:dadosAutorizacao"]["ans:dataValidadeSenha"]._text !=
+        dataDaValidadeDaSenha
+    ) {
+      guide["ans:dadosAutorizacao"]["ans:dataValidadeSenha"]._text =
+        dataDaValidadeDaSenha || "";
+    }
+
+    if (guide["ans:procedimentosExecutados"]) {
+      guide["ans:procedimentosExecutados"]["ans:procedimentoExecutado"].map(
+        (proc, idx) => {
+          const {
+            tabelaProcedimento,
+            codigoProcedimento,
+            descricaoProcedimento,
+            dataProcedimento,
+            quantidadeProcedimento,
+            valorUnitarioProcedimento,
+            valorProcedimento,
+          } = procedimentos[idx];
+
+          if (
+            proc["ans:procedimento"]["ans:codigoTabela"]._text !=
+            tabelaProcedimento
+          ) {
+            proc["ans:procedimento"]["ans:codigoTabela"]._text =
+              tabelaProcedimento;
+          }
+
+          if (
+            proc["ans:procedimento"]["ans:codigoProcedimento"]._text !=
+            codigoProcedimento
+          ) {
+            proc["ans:procedimento"]["ans:codigoProcedimento"]._text =
+              codigoProcedimento;
+          }
+
+          if (
+            proc["ans:procedimento"]["ans:descricaoProcedimento"]._text !=
+            descricaoProcedimento
+          ) {
+            proc["ans:procedimento"]["ans:descricaoProcedimento"]._text =
+              descricaoProcedimento;
+          }
+
+          if (proc["ans:dataExecucao"]._text != dataProcedimento) {
+            proc["ans:dataExecucao"]._text = dataProcedimento;
+          }
+
+          if (
+            proc["ans:quantidadeExecutada"]._text !=
+            quantidadeProcedimento
+          ) {
+            proc["ans:quantidadeExecutada"]._text =
+              quantidadeProcedimento;
+          }
+
+          if (
+            proc["ans:valorUnitario"]._text !=
+            valorUnitarioProcedimento
+          ) {
+            proc["ans:valorUnitario"]._text =
+              valorUnitarioProcedimento;
+          }
+          if (
+            proc["ans:valorTotal"]._text !=
+            valorUnitarioProcedimento
+          ) {
+            proc["ans:valorTotal"]._text =
+              codigoProcedimento;
+          }
+        }
+      );
+    }
+
+    setArquives((prev) => {
+      const updatedArquives = [...prev];
+      updatedArquives[idxArquive].objectXml["ans:mensagemTISS"][
+        "ans:prestadorParaOperadora"
+      ]["ans:loteGuias"]["ans:guiasTISS"]["ans:guiaSP-SADT"][idxGuide] = guide;
+      return updatedArquives;
+    });
+  }
+
   return (
-    <ContextXml.Provider value={{ arquives, AddNewArquive }}>
+    <ContextXml.Provider
+      value={{ arquives, AddNewArquive, UpdateGuideDetails }}
+    >
       {children}
     </ContextXml.Provider>
   );

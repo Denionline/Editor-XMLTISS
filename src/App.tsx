55ceac4df1f66a079a34cefe2324790a1f45d971
Denionline <danielximenes164@gmail.com>
2025-05-24 11:48:28 +0100

    fix: Fix the import from next to react

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import {Analytics} from "@vercel/analytics/next";
+import {Analytics} from "@vercel/analytics/react";
 import {RouterProvider} from "react-router-dom";
 import {ThemeProvider} from "./components/theme-provider";
 import {ContextXmlProvider} from "./context/ContextXml";
